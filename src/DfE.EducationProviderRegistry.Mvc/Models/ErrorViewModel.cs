438631286abab79fa3fa0e728688c8cfcf64b273
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2026-05-29 11:02:47 +0100

    initial web and mvc project scaffold

---
+++ src/DfE.EducationProviderRegistry.Mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DfE.EducationProviderRegistry.Mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

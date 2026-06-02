53a30fafecea803d141990d5aea5d0e3e6ac6a26
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2026-06-02 08:23:45 +0100

    added not found page and routing template

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/ErrorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Controllers;
+
+public class ErrorController : Controller
+{
+    [Route("not-found")]
+    public IActionResult NotFoundPage()
+    {
+        Response.StatusCode = 404;
+        return View("NotFound");
+    }
+}

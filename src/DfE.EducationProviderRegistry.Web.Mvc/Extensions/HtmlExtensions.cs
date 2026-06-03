602a6a055a39bf2e84fbbc9d97c06072cc33b34c
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2026-06-03 13:25:37 +0100

    UI navigation and layout partials

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Extensions/HtmlExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Extensions
+{
+    public static class HtmlExtensions
+    {
+        public static string ActiveClassForPath(this IHtmlHelper html, string routePrefix)
+        {
+            var path = html.ViewContext.HttpContext.Request.Path.Value?.ToLower() ?? "";
+            routePrefix = routePrefix.ToLower();
+
+            if (path == routePrefix || path.StartsWith(routePrefix + "/"))
+            {
+                return "govuk-service-navigation__item--active";
+            }
+
+            return string.Empty;
+        }
+    }
+}

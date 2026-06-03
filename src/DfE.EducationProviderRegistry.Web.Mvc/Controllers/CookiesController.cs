1ec6410cb1465315703ee2189e47fce34a149c2a
TonyPalmerDfE <tony.palmer@education.gov.uk>
2026-06-03 16:47:22 +0100

    cookie analytics and partial templates added (#5)

--- src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/CookiesController.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/CookiesController.cs
@@ -8,4 +8,27 @@ public class CookiesController : Controller
     {
         return View();
     }
+
+    [HttpPost]
+    public IActionResult SetPreferences(string cookies_analytics)
+    {
+        bool analyticsAccepted = cookies_analytics == "yes";
+
+        Response.Cookies.Append(
+            "cookies_analytics",
+            analyticsAccepted ? "yes" : "no",
+            new CookieOptions
+            {
+                Expires = DateTimeOffset.UtcNow.AddDays(30),
+                Secure = true,
+                HttpOnly = false,
+                SameSite = SameSiteMode.Strict
+            });
+
+        string referer = Request.Headers.Referer.ToString();
+        if (!string.IsNullOrWhiteSpace(referer))
+            return Redirect(referer);
+
+        return RedirectToAction("Index", "Home");
+    }
 }

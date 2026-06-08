131b8fb122bdcf4f2b8002f4515ab902e6113d42
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2026-06-08 11:33:21 +0100

    add health check endpoint

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/HealthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Controllers
+{
+    public class HealthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Ok(new
+            {
+                status = "Service is running",
+                timestamp = DateTime.UtcNow
+            });
+        }
+    }
+}

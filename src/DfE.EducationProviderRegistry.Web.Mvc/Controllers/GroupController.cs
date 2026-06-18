ae8f9c481bcfb0cdda0f4f91dc0bc02c605c8385
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-06-18 09:57:31 +0100

    add query package (#10)

--- src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/GroupController.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Controllers/GroupController.cs
@@ -3,54 +3,54 @@ using DfE.EducationProviderRegistry.Web.Mvc.ApplicationDtos;
 using DfE.EducationProviderRegistry.Web.Mvc.ViewModels.Pages;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DfE.EducationProviderRegistry.Web.Mvc.Controllers
+namespace DfE.EducationProviderRegistry.Web.Mvc.Controllers;
+
+public class GroupController : Controller
 {
-    public class GroupController : Controller
+    private readonly IMapper<GroupDto, GroupDetailsPageViewModel> _groupDetailsPageMapper;
+
+    public GroupController(
+        IMapper<GroupDto, GroupDetailsPageViewModel> groupDetailsMaper)
     {
-        private readonly IMapper<GroupDto, GroupDetailsPageViewModel> _groupDetailsPageMapper;
+        ArgumentNullException.ThrowIfNull(groupDetailsMaper);
+        _groupDetailsPageMapper = groupDetailsMaper;
+    }
 
-        public GroupController(
-            IMapper<GroupDto, GroupDetailsPageViewModel> groupDetailsMaper)
+    [HttpGet("/group/{code}")]
+    public IActionResult Details(string code)
+    {
+        GroupDto applicationDtoDummy = new()
         {
-            _groupDetailsPageMapper = groupDetailsMaper;
-        }
-
-        [HttpGet("/group/{code}")]
-        public IActionResult Details(string code)
-        {
-            GroupDto applicationDtoDummy = new()
+            BasicDetails = new GroupBasicDetailsDto
             {
-                BasicDetails = new GroupBasicDetailsDto
-                {
-                    Name = "Erdington Trust",
-                    Uid = "T001",
-                    GroupId = code,
-                    Ukprn = "12345678",
-                    CompaniesHouseNumber = "09876543",
-                    Status = "Open",
-                    Address = "123 Trust Road, Birmingham",
-                    Type = "Multi-academy trust"
-                },
-                Academies = new List<GroupAcademiesDto>
-                {
-                    new() { Name = "St Mary's Primary", Urn = "100001" },
-                    new() { Name = "St John's Academy", Urn = "100002" }
-                },
-                Trustees = new List<GroupTrusteesDto>
-                {
-                    new() { Name = "Alice Brown", GovernorId = "G001", Role = "Chair", StartDate = "01 Jan 2020" },
-                    new() { Name = "David Smith", GovernorId = "G002", Role = "Trustee", StartDate = "15 Mar 2021" }
-                },
-                Members = new List<GroupMembersDto>
-                {
-                    new() { Name = "Sarah Green", GovernorId = "G001", StartDate = "10 Feb 2019" },
-                    new() { Name = "John White", GovernorId = "G002", StartDate = "22 Jun 2020" }
-                }
-            };
+                Name = "Erdington Trust",
+                Uid = "T001",
+                GroupId = code,
+                Ukprn = "12345678",
+                CompaniesHouseNumber = "09876543",
+                Status = "Open",
+                Address = "123 Trust Road, Birmingham",
+                Type = "Multi-academy trust"
+            },
+            Academies = new List<GroupAcademiesDto>
+            {
+                new() { Name = "St Mary's Primary", Urn = "100001" },
+                new() { Name = "St John's Academy", Urn = "100002" }
+            },
+            Trustees = new List<GroupTrusteesDto>
+            {
+                new() { Name = "Alice Brown", GovernorId = "G001", Role = "Chair", StartDate = "01 Jan 2020" },
+                new() { Name = "David Smith", GovernorId = "G002", Role = "Trustee", StartDate = "15 Mar 2021" }
+            },
+            Members = new List<GroupMembersDto>
+            {
+                new() { Name = "Sarah Green", GovernorId = "G001", StartDate = "10 Feb 2019" },
+                new() { Name = "John White", GovernorId = "G002", StartDate = "22 Jun 2020" }
+            }
+        };
 
-            GroupDetailsPageViewModel model = _groupDetailsPageMapper.Map(applicationDtoDummy);
+        GroupDetailsPageViewModel model = _groupDetailsPageMapper.Map(applicationDtoDummy);
 
-            return View(model);
-        }
+        return View(model);
     }
 }

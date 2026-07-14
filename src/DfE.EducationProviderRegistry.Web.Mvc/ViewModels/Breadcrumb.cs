4611b613b76005828c9f410bd45cfe863c3886fc
Dan Owen <daniel.owen@education.gov.uk>
2026-07-14 13:53:01 +0100

    Breadcrumb addition

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/ViewModels/Breadcrumb.cs
@@ -0,0 +1,7 @@
+﻿namespace DfE.EducationProviderRegistry.Web.Mvc.ViewModels
+{
+    public sealed record Breadcrumb(
+        string Text,
+        string? Controller = null,
+        string? Action = null);
+}
---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Views/Shared/_Breadcrumbs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Views.Shared
+{
+    public class BreadcumbsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

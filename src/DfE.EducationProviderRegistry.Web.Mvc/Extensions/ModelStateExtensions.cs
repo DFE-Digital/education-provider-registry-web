8767696d0ae9e76909d9cae636d5cbaf9b0cd41a
Tony Palmer <Tony.PALMER@EDUCATION.GOV.UK>
2026-07-14 13:54:35 +0100

    model state extension to help with validation

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Extensions;
+
+public static class ModelStateExtensions
+{
+    public static bool HasErrorFor(this ViewDataDictionary viewData, string key)
+    {
+        return viewData.ModelState.TryGetValue(key, out var entry)
+            && entry.Errors.Count > 0;
+    }
+}

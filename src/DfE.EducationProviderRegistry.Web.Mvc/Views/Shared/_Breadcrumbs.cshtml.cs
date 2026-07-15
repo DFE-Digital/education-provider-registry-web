8da98538de93968098b63362e98cd7a7981940d0
Dan Owen <daniel.owen@education.gov.uk>
2026-07-15 13:35:26 +0100

    Remove empty method

--- src/DfE.EducationProviderRegistry.Web.Mvc/Views/Shared/_Breadcrumbs.cshtml.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Views/Shared/_Breadcrumbs.cshtml.cs
@@ -1,12 +1,9 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace DfE.EducationProviderRegistry.Web.Mvc.Views.Shared
 {
     public class BreadcumbsModel : PageModel
     {
-        public void OnGet()
-        {
-        }
+
     }
 }

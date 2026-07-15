1d19dcedb057988b38de1eb38ec763a824f3c368
Dan Owen <daniel.owen@education.gov.uk>
2026-07-15 13:21:42 +0100

    Update breadcrumb view model to be inline with naming conventions Add unit test for coverage

--- src/DfE.EducationProviderRegistry.Web.Mvc/ViewModels/BreadcrumbViewModel.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/ViewModels/BreadcrumbViewModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace DfE.EducationProviderRegistry.Web.Mvc.ViewModels
 {
-    public sealed record Breadcrumb(
+    public sealed record BreadcrumbViewModel(
         string Text,
         string? Controller = null,
         string? Action = null);
---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.UnitTests/Features/Search/ViewModels/BreadcrumbViewModelUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using DfE.EducationProviderRegistry.Web.Mvc.ViewModels;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.UnitTests.Features.Search.ViewModels;
+
+public sealed class BreadcrumbViewModelUnitTests
+{
+    [Fact]
+    public void Constructor_SetsPropertiesCorrectly()
+    {
+        // arrange
+        string text = "test";
+        string controller = "controller";
+        string action = "action";
+
+        // act
+        BreadcrumbViewModel vm = new(text, controller, action);
+
+        // assert
+        Assert.Equal(text, vm.Text);
+        Assert.Equal(controller, vm.Controller);
+        Assert.Equal(action, vm.Action);
+    }
+
+    [Fact]
+    public void Constructor_AllowsNullCount()
+    {
+        // arrange
+        string text = "test";
+
+        // act
+        BreadcrumbViewModel vm = new(text);
+
+        // assert
+        Assert.Null(vm.Action);
+        Assert.Null(vm.Controller);
+        Assert.Equal("test", vm.Text);
+    }
+}

72baf5b194ee9110a734c9f5d343b57060a9aa72
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-07-30 18:11:14 +0100

    use container network for connectionStringBuilder going container-container in v1.0.16

--- tests/DfE.EducationProviderRegistry.Web.Mvc.AccessibilityTests/ApplicationHostedEnvironment.cs
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.AccessibilityTests/ApplicationHostedEnvironment.cs
@@ -1,5 +1,6 @@
 ﻿using DfE.Core.Libraries.IntegrationTests.Abstractions;
 using DfE.Core.Libraries.IntegrationTests.Database.Abstractions;
+using DfE.Core.Libraries.IntegrationTests.Database.Postgres.Container;
 using DfE.EducationProviderRegistry.Web.Mvc.AccessibilityTests.Options;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Configurations;
@@ -15,16 +16,22 @@ public sealed class ApplicationHostedEnvironment
     private IContainer? _applicationContainer;
     private readonly IDatabaseFactory _databaseFactory;
     private readonly ApplicationHostOptions _options;
+    private readonly ContainerOptions _dbContainerOptions;
+    private readonly PostgresDatabaseOptions _dbOptions;
     private readonly INetwork _containerNetwork;
 
     public ApplicationHostedEnvironment(
         IDatabaseFactory databaseFactory,
         ApplicationHostOptions options,
-        INetwork containerNetwork)
+        INetwork containerNetwork,
+        ContainerOptions dbContainerOptions,
+        PostgresDatabaseOptions dbOptions)
     {
         _databaseFactory = databaseFactory;
         _options = options;
         _containerNetwork = containerNetwork;
+        _dbContainerOptions = dbContainerOptions;
+        _dbOptions = dbOptions;
     }
 
     public async Task InitialiseAsync(
@@ -32,13 +39,21 @@ public sealed class ApplicationHostedEnvironment
     {
         _database = await _databaseFactory.CreateAsync(ct);
 
-        NpgsqlConnectionStringBuilder builder = new(_database.ConnectionString);
-        builder.Port = 5432;
+        const int postgresContainerPort = 5432;
+
+        NpgsqlConnectionStringBuilder containerNetworkConnectionStringBuilder = new()
+        {
+            Host = _dbContainerOptions.HostName,
+            Port = postgresContainerPort, // Internal postgres port as container-container
+            Database = _dbOptions.Database,
+            Username = _dbOptions.Username,
+            Password = _dbOptions.Password
+        };
 
         _applicationContainer =
             new ContainerBuilder(_options.Container.Image)
                 .WithExposedPorts<ContainerBuilder, IContainer, IContainerConfiguration>(_options.Container.PortMappings ?? [])
-                .WithEnvironment("eprweb_eprdat_dotnet_db_connection", builder.ConnectionString)
+                .WithEnvironment("eprweb_eprdat_dotnet_db_connection", containerNetworkConnectionStringBuilder.ConnectionString)
                 .WithWaitStrategy(Wait.ForUnixContainer().UntilHttpRequestIsSucceeded(r => r.ForPort((ushort)_options.Container.PortMappings!.First().ContainerPort)))
                 .WithNetwork(_containerNetwork)
                 .Build();

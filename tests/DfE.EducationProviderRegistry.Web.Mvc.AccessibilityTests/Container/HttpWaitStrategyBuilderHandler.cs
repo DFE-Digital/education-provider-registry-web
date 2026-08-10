f80b31fffc418ef7a5ad574c2d6ca9e364051df7
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-08-10 21:52:15 +0100

    [integration-tests] consume CoreLibs .24 extends Container creation to register runtime builders (#74)
    * consume .24
    
    * remove disposable

---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.AccessibilityTests/Container/HttpWaitStrategyBuilderHandler.cs
@@ -0,0 +1,31 @@
+﻿using DfE.Core.Libraries.IntegrationTests.Abstractions.Containers.Registry;
+using Microsoft.Extensions.Options;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.AccessibilityTests.Container;
+
+internal sealed class HttpWaitStrategyBuilderHandler : IContainerBuilderHandler<ContainerBuilder>
+{
+    private readonly ContainerOptions _options;
+
+    public HttpWaitStrategyBuilderHandler(
+        IOptionsMonitor<ContainerOptions> options)
+    {
+        _options = options.Get("epr-web");
+    }
+
+    public ValueTask<ContainerBuilder> ApplyAsync(
+        ContainerBuilder builder,
+        CancellationToken cancellationToken)
+    {
+        return ValueTask.FromResult(
+            builder.WithWaitStrategy(
+                Wait.ForUnixContainer()
+                    .UntilHttpRequestIsSucceeded(
+                        request =>
+                            request.ForPort(
+                                (ushort)_options
+                                    .PortMappings!
+                                    .First()
+                                    .ContainerPort))));
+    }
+}

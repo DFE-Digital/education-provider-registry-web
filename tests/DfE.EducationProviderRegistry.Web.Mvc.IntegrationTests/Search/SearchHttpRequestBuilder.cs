6d3a718a62b39143b95825dc13c37f85da732641
aahmed-dfe <119260940+aahmed-dfe@users.noreply.github.com>
2026-08-26 20:15:53 +0100

    use async connectionstring provider (#96)

---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.IntegrationTests/Search/SearchHttpRequestBuilder.cs
@@ -0,0 +1,49 @@
+﻿namespace DfE.EducationProviderRegistry.Web.Mvc.IntegrationTests.Search;
+
+public sealed class SearchHttpRequestBuilder
+{
+    private readonly SearchPanel _searchPanel;
+    private string? _identityTerm;
+    private string? _locationTerm;
+
+    public SearchHttpRequestBuilder(SearchPanel searchPanel)
+    {
+        _searchPanel = searchPanel;
+    }
+
+    public SearchHttpRequestBuilder WithIdentitySearchTerm(string value)
+    {
+        _identityTerm = value;
+        return this;
+    }
+
+    public SearchHttpRequestBuilder WithLocationTerm(string value)
+    {
+        _locationTerm = value;
+        return this;
+    }
+
+    public HttpRequestMessage Build()
+    {
+        Dictionary<string, string> values = [];
+
+        if (_identityTerm is not null)
+        {
+            values.Add(_searchPanel.GetIdentityInputName(), _identityTerm);
+        }
+
+        if (_locationTerm is not null)
+        {
+            values.Add(_searchPanel.GetLocationInputName(), _locationTerm);
+        }
+
+        (HttpMethod method, Uri target) = _searchPanel.GetFormDetails();
+
+        return new HttpRequestMessage(method, target)
+        {
+            Content = new FormUrlEncodedContent(values)
+        };
+    }
+
+    public static SearchHttpRequestBuilder Create(SearchPanel panel) => new(panel);
+}

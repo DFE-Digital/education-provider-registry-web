f87ba9cce392006c34541364315ae7fa911978ac
aahmed-dfe <Aasim.AHMED@EDUCATION.GOV.UK>
2026-09-01 08:11:34 +0100

    fix db connectionStringHandler and pass networkName. add test for searchSort

---
+++ tests/DfE.EducationProviderRegistry.Web.MVC.UITests/Search/SearchResults.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DfE.EducationProviderRegistry.Web.MVC.UITests.Search;
+
+public sealed class SearchResults
+{
+    private readonly WebDriverWait _defaultWaiter;
+    private readonly IWebDriver _driver;
+
+    private static By ResultRecords => By.CssSelector("#establishments .govuk-table");
+    private static By ResultName => By.CssSelector(".govuk-table__caption");
+
+    public SearchResults(IWebDriver driver)
+    {
+        _driver = driver;
+        _defaultWaiter = new(_driver, TimeSpan.FromSeconds(15));
+    }
+
+    public IReadOnlyCollection<SearchResult> GetSearchResults()
+    {
+        IReadOnlyCollection<IWebElement>? results = _defaultWaiter.Until(driver =>
+        {
+            var elements = driver.FindElements(ResultRecords);
+            return elements.Count > 0 ? elements : null;
+        });
+
+        return [
+            .. results?.Select(
+                (result) =>
+                {
+                    string name = result.FindElement(ResultName).Text;
+                    return new SearchResult(name);
+                }) ?? []];
+    }
+}
+
+public sealed record SearchResult(string Name);

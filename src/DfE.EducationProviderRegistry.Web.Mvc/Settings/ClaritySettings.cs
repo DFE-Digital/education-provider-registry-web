f0e50e809af979fe8b6e29f831d3d56815827d03
Spencer O'Hegarty <49732575+spanersoraferty@users.noreply.github.com>
2026-09-03 09:38:17 +0100

    So/gias 489 ms clarity setup (#101)
    * Initial commit
    
    * Wire though of clarity and cookie acceptance
    
    * Removed redundant controller action from cookies controller
    
    * Added cookies controller tests and supporting test doubles
    
    * removed loacl connection details
    
    * Added secure = true and HttpOnly = true fro cookie options (SonarQube recommended).
    
    * dotnet format
    
    * Added disposal for JsonDocument as per Tony's PR recommendation
    
    * Removed comments from cookies chtml as per Tony's PR comments.
    
    * Updated cookies banner to route to correct controller action
    
    * Ignore failing UI tests (Aasmin to forward fix)
    
    * Revert "Ignore failing UI tests (Aasmin to forward fix)"
    
    This reverts commit ab9ef808e79e09b54e3aa488fa26bb5e18f0d2cc.
    
    ---------
    
    Co-authored-by: Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
    Co-authored-by: aahmed-dfe <aasim.ahmed@education.gov.uk>

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Settings/ClaritySettings.cs
@@ -0,0 +1,8 @@
+﻿namespace DfE.EducationProviderRegistry.Web.Mvc.Settings;
+
+public class ClaritySettings
+{
+    public bool Enabled { get; set; }
+    public string ProjectId { get; set; } = string.Empty;
+}
+

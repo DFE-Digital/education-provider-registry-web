a5c8d79c22f3e47307cae0ba1fd38f86d76dbac0
Dan Owen <daniel.owen@education.gov.uk>
2026-09-09 15:34:01 +0100

    Remove duplicated code from google scripts

--- src/DfE.EducationProviderRegistry.Web.Mvc/Settings/GoogleAnalyticsSettings.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Settings/GoogleAnalyticsSettings.cs
@@ -1,4 +1,5 @@
-﻿namespace DfE.EducationProviderRegistry.Web.Mvc.Settings;
+﻿using DfE.EducationProviderRegistry.Web.Mvc.Extensions;
+namespace DfE.EducationProviderRegistry.Web.Mvc.Settings;
 
 public class GoogleAnalyticsSettings
 {
@@ -7,4 +8,22 @@ public class GoogleAnalyticsSettings
     public string? AuthenticationId { get; set; }
 
     public string? PreviewValue { get; set; }
+
+    public string Domain { get; } = "https://www.googletagmanager.com";
+
+    public string TagManagerQueryString =>
+        (!string.IsNullOrWhiteSpace(AuthenticationId) && !string.IsNullOrWhiteSpace(PreviewValue)
+            ? $"&gtm_auth={Uri.EscapeDataString(AuthenticationId)}" +
+              $"&gtm_preview={Uri.EscapeDataString(PreviewValue)}" +
+              "&gtm_cookies_win=x"
+            : string.Empty
+        );
+
+    public bool IsGoogleTagManagerEnabled(HttpContext context)
+    {
+        ArgumentNullException.ThrowIfNull(context);
+
+        return !string.IsNullOrWhiteSpace(ContainerId)
+            && context.IsAnalyticsConsentGranted();
+    }
 }

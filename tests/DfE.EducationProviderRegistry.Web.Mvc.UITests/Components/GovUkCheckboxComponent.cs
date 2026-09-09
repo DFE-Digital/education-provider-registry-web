5bfc38ff26c22af65de1e7afc400a4817d2d43e2
aahmed-dfe <aasim.ahmed@education.gov.uk>
2026-09-09 09:14:00 +0100

    back onto govukcomponents

---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.UITests/Components/GovUkCheckboxComponent.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DfE.EducationProviderRegistry.Web.MVC.UITests.Components;
+
+internal sealed class GovUkCheckboxComponent
+{
+    private readonly WebDriverWait _wait;
+    public GovUkCheckboxComponent(IWebDriver driver)
+    {
+        _wait = new(driver, TimeSpan.FromSeconds(15));
+    }
+    // TODO don't rich model e.g. Checkbox (label, value, map yet....
+    // all checkbox components returned - Client forced to filter for which checkbox component they want on page and map
+    public IReadOnlyCollection<IWebElement> Find()
+    {
+        return _wait.Until((driver) =>
+            driver.FindElements(
+                By.CssSelector(".govuk-checkboxes")));
+    }
+
+    // how do I find which checkbox I want to click
+    public void Click(string text)
+    {
+        _wait.Until((driver) =>
+        {
+            IReadOnlyCollection<IWebElement> all = Find();
+
+            IReadOnlyCollection<IWebElement> labelsThatMatches =
+                all.Select((checkboxComponent) => checkboxComponent.FindElement(By.CssSelector(".govuk-checkboxes__label")))
+                    .Where((label) => label.Text.Contains(text, StringComparison.OrdinalIgnoreCase))
+                    .ToList();
+
+            if (labelsThatMatches.Count == 0)
+            {
+                throw new InvalidOperationException($"No checkboxes found with label text {text}");
+            }
+
+            if (labelsThatMatches.Count > 1)
+            {
+                throw new ArgumentException($"Multiple click targets available with text {text}");
+            }
+
+            labelsThatMatches.Single().Click();
+        });
+    }
+}
---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.UITests/Components/GovUkDetailsComponent.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace DfE.EducationProviderRegistry.Web.MVC.UITests.Components;
+
+public sealed class GovUkDetailsComponent
+{
+    private readonly WebDriverWait _wait; 
+    public GovUkDetailsComponent(IWebDriver webDriver)
+    {
+        ArgumentNullException.ThrowIfNull(webDriver);
+        _wait = new(webDriver, TimeSpan.FromSeconds(15));
+    }
+
+    public IReadOnlyCollection<IWebElement> Find()
+    {
+        return _wait.Until((driver) 
+            => driver.FindElements(By.CssSelector(".govuk-details")));
+    }
+
+    public void Expand(string text)
+    {
+        _wait.Until((driver) =>
+        {
+            Func<IReadOnlyList<IWebElement>> matchingDetails = 
+                () =>
+                    Find()
+                        .Where((details) => 
+                            details.FindElement(By.CssSelector(".govuk-details__summary-text")).Text
+                                .Contains(text, StringComparison.OrdinalIgnoreCase))
+                        .ToList();
+
+            IReadOnlyList<IWebElement> locatedMatchingDetails = matchingDetails();
+
+            // better errors than .Single()
+            if(locatedMatchingDetails.Count == 0)
+            {
+                throw new ArgumentException($"Could not locate GovUkDetails with text {text}");
+            }
+
+            if(locatedMatchingDetails.Count > 1)
+            {
+                throw new InvalidOperationException($"Located multiple GovUkDetails with text {text}");
+            }
+
+            locatedMatchingDetails
+                .Single()
+                .FindElement(By.CssSelector(".govuk-details__summary-text"))
+                .Click();
+
+            // verify expanded target with "open" on details
+            bool result = matchingDetails().Single().GetAttribute("open") is not null;
+
+            return result;
+        });
+    }
+}

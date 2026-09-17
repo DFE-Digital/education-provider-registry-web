03cc75a47d43fbd877bbd6331a7f54508f2237ec
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2026-09-17 10:06:34 +0100

    Initial scaffoled for download controller

---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Features/Download/Controllers/DownloadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.Features.Download.Controllers;
+
+[Route("download")]
+public sealed class DownloadController : Controller
+{
+    [HttpGet("")]
+    public IActionResult Index()
+    {
+        return View("Index");
+    }
+}

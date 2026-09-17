2750674ff3c7e137142fd3251272879633421b27
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2026-09-17 10:26:15 +0100

    Added scaffold for download unit tests.

---
+++ tests/DfE.EducationProviderRegistry.Web.Mvc.UnitTests/Features/Download/Controllers/DownloadControllerUnitTests.cs
@@ -0,0 +1,22 @@
+using DfE.EducationProviderRegistry.Web.Mvc.Features.Download.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DfE.EducationProviderRegistry.Web.Mvc.UnitTests.Features.Download.Controllers;
+
+public sealed class DownloadControllerUnitTests
+{
+    [Fact]
+    public void Index_ReturnsCorrectViewAndModel()
+    {
+        // arrange
+
+        DownloadController sut = new();
+
+        // act
+        IActionResult result = sut.Index();
+
+        // assert
+        ViewResult view = Assert.IsType<ViewResult>(result);
+        Assert.Equal("Index", view.ViewName);
+    }
+}

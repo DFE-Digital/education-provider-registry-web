69339f7fde274fed484b2da6e020701fe43f283f
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2026-09-17 13:48:59 +0100

    Scaffolded full down journey

--- src/DfE.EducationProviderRegistry.Web.Mvc/Features/Download/Controllers/DownloadController.cs
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Features/Download/Controllers/DownloadController.cs
@@ -1,13 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DfE.EducationProviderRegistry.Web.Mvc.Features.Download.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DfE.EducationProviderRegistry.Web.Mvc.Features.Download.Controllers;
 
 [Route("download")]
 public sealed class DownloadController : Controller
 {
-    [HttpGet("")]
+    [HttpGet]
     public IActionResult Index()
     {
         return View("Index");
+    }
+
+    [AcceptVerbs("GET", "POST")]
+    [Route("download/downloading")]
+    public IActionResult Downloading([FromForm] DownloadViewModel? model)
+    {
+        if (HttpContext.Request.Method == "POST")
+        {
+            return View(model);
+        }
+
+        return View(model);
+    }
+
+    [HttpGet("complete")]
+    public IActionResult Complete()
+    {
+        return View();
     }
 }
---
+++ src/DfE.EducationProviderRegistry.Web.Mvc/Features/Download/ViewModels/DownloadViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace DfE.EducationProviderRegistry.Web.Mvc.Features.Download.ViewModels;
+
+public sealed class DownloadViewModel
+{
+}
